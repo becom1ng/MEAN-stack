44059b465eab30428ace5ed9e6e7b4625357a70c
becom1ng <becom1ng@users.noreply.github.com>
2024-07-25 13:13:59 -0500

    Add: http with fetch, employee-list-component, routes for lazy loading, basic styling.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <mat-toolbar>
+      <span>Employees Management System</span>
+    </mat-toolbar>
+    <main>
+      <router-outlet />
+    </main>
   `,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'client';
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'employees',
+    loadComponent: () =>
+      import('./employee/employee-list/employee-list.component').then(
+        (m) => m.EmployeeListComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'employees',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'employees',
+    pathMatch: 'full',
+  },
+];
