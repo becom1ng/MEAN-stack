35214669d65a9c682a5fdab1b6a8f36fdcfc8d69
becom1ng <becom1ng@users.noreply.github.com>
2024-07-25 10:27:27 -0500

    Remove prior app. Added new structure and foundation.

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { connectToDatabase } from './database';
+
+// Load environment variables from the .env file, where the LOCAL_URI is configured
+dotenv.config();
+
+const { LOCAL_URI } = process.env;
+
+if (!LOCAL_URI) {
+	console.error(
+		'No LOCAL_URI environment variable has been defined in config.env',
+	);
+	process.exit(1);
+}
+
+connectToDatabase(LOCAL_URI)
+	.then(() => {
+		const app = express();
+		app.use(cors());
+
+		// start the Express server
+		app.listen(5200, () => {
+			console.log(`Server running at http://localhost:5200...`);
+		});
+	})
+	.catch((error) => console.error(error));
